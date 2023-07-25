9d562b039dbb49f3412dfc02b303a21121e82e4a
mattia0419 <pironemattia6@gmail.com>
2023-07-25 16:56:42 +0200

    add JSnack3

---
+++ main.js
@@ -0,0 +1,12 @@
+const num = [45, 78, 33, 9, 4, 12, 34, 66, 70];
+const result = document.getElementById("result");
+let val = 0;
+
+for(let i = 0; i < num.length; i++){
+    if(i % 2 != 0){
+        val += num[i];
+        console.log(val);
+    }
+    result.innerHTML = val;
+}
+
